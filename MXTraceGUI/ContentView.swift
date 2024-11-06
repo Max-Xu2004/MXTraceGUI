787e8951299ef54846eca3d15db52b2cb4b00f8b
Max Xu <xujinjia@redrock.team>
2024-11-06 13:40:01 +0800

    Initial Commit

---
+++ MXTraceGUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MXTraceGUI
+//
+//  Created by Max Xu on 2024/11/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MXTraceGUI/MXTraceGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MXTraceGUIApp.swift
+//  MXTraceGUI
+//
+//  Created by Max Xu on 2024/11/6.
+//
+
+import SwiftUI
+
+@main
+struct MXTraceGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
